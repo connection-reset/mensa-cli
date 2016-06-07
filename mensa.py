023ec947743011548cf959ac04b0eeea0bf5d346
Axel <connection-reset@users.noreply.github.com>
2016-06-07 13:04:06 +0200

    Get todays food plan for the canteen at blvd from stw-bremen.de
    stw-bremen.de loads faster than uni-bremen.de but shows the food plan
    for the next 5 days, not the current week (see also https://github.com/albalitz/mensa-cli/issues/1).
    I'm only interested in todays plan anyway.

--- mensa.py
+++ mensa.py
@@ -1,84 +1,35 @@
 #!/usr/bin/env python3
-import argparse
-import urllib.request
+import copy
+import itertools
 from bs4 import BeautifulSoup
-import re
+import requests
 
-_MENSA_URL = "http://www.uni-bremen.de/service/taeglicher-bedarf/essen-auf-dem-campus/cafeteria/390"
+def normalize(string):
+    generator = map(str.strip, string.splitlines())
+    line_parts = list()
+    while True:
+        l = list(itertools.takewhile(lambda x: x != "", generator))
+        if len(l) == 0:
+            break
+        line_parts.append(" ".join(l))
+    return ", ".join(line_parts)
 
+def clean(html):
+    html = copy.copy(html)
+    for tag in html.find_all("sup"):
+        tag.decompose()
+    return html
 
-def _clean(html):
-    clean = html.replace("<br>", "\n").replace("</br>", "")
-    clean = clean.replace("&amp;", "&")
-    clean = clean.replace("&gt;", ">").replace("&lt;", "<")
-    clean = clean.replace("\n&\n", " & ").replace("\n&", " &")
-
-    clean = clean.replace("<strong>", "{} ".format("+" * 4)).replace("</strong>", " {}".format("+" * 4))
-    clean = re.sub(r'\n{3,}', "\n\n", clean)
-    clean = re.sub(r' +', " ", clean)
-    clean = re.sub(r' ?\n ?(an|in|mit|dazu) ?\n?', r' \1 ', clean)
-    return clean
-
-def _get_mensa_info():
-    result = urllib.request.urlopen(_MENSA_URL)
-    html = result.read()
-
-    soup = BeautifulSoup(html, "html.parser")
-
-    main_div = soup.html.find("div", class_="tx-hbucafeteria-pi1")
-    main_heading = main_div.find("h2")
-    day_trs = main_div.find("table").find_all("tr")[1:]
-    hours_div = main_div.find_all("div", class_="tx-hbucafeteria-pi1-info")[1]
-
-    return (main_heading.string, day_trs, hours_div)
-
-
-def _get_hours():
-    heading, meal_trs, hours_div = _get_mensa_info()
-    strings = [str(tag) for tag in hours_div.contents[2:]]
-    return _clean("".join(strings))
-
-
-def _get_menu():
-    heading, day_trs, hours_div = _get_mensa_info()
-
-    days_and_meals = []
-    for day_tr in day_trs:
-        tds = day_tr.find_all("td")
-
-        day = tds[0]
-        meal_tds = tds[1].contents[1:]
-        meal_tds_strings = [str(tag) for tag in meal_tds]
-
-        meals = _clean("".join(meal_tds_strings))
-
-        days_and_meals.append((day.string, meals))
-
-    return (heading, days_and_meals)
-
-
-
+def get_food_plan():
+    r = requests.get("http://www.stw-bremen.de/de/essen-trinken/uni-mensa")
+    r.raise_for_status()
+    soup = BeautifulSoup(r.text, "html.parser")
+    food_plan = soup.find(id="food-plan-0")
+    categories = food_plan.find_all(class_="food-category")
+    for category in categories:
+        title = category.find(class_="category-name")
+        meals = category.find_all(class_="field-name-field-description")
+        yield (title.get_text(), [normalize(clean(meal).get_text()) for meal in meals])
 
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser(description="Mensa menu from your terminal.")
-    argparser.add_argument("--open", "-o",
-                           action="store_true",
-                           help="Show opening hours.")
-    args = argparser.parse_args()
-
-    if args.open:
-        hours = _get_hours()
-        print(hours)
-        quit()
-
-    else:
-        heading, menu = _get_menu()
-        print(heading)
-        for day, meals in menu:
-            print("-" * (10 + len(day)))
-            print("-{}{}{}-".format(" " * 4, day, " " * 4))
-            print("-" * (10 + len(day)))
-
-            print(meals)
-
-        quit()
+    print("\n\n".join(["{:-^80}\n{}".format(cat, "\n".join(meals)) for cat, meals in get_food_plan()]))
